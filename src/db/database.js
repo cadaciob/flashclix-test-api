6e9d1579283a634e0678f72df55d1b859c7b87b0
roadtodiploma <roadtodiploma@roadtodiplomas-MacBook-Pro.local>
2020-05-05 01:56:20 +0200

    changed to mongoDB atlas

--- src/db/database.js
+++ src/db/database.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/flashclix-api', {
+// mongodb+srv://htlwienwesterUser:htlwienwestPass@cluster0-6kjju.mongodb.net/flashclix-test-api2?retryWrites=true&w=majority
+
+const db = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/flashclix-api'
+
+mongoose.connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
