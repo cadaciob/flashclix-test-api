22d51bf12127932d88ce247a2f063ca283506996
roadtodiploma <roadtodiploma@roadtodiplomas-MacBook-Pro.local>
2020-05-16 17:44:23 +0200

    x-origin Authorization update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
   next()
 })
